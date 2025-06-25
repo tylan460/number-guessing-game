72cde41c142d034c24930a7d2190f715a5836974
@tyvanrjhb024 <tyvanrjhb024@student.wethinkcode.co.za>
2025-06-25 23:49:55 +0200

    more dom manipulation

--- logic.js
+++ logic.js
@@ -2,5 +2,32 @@
 
 let random = Math.floor(Math.random() * (20 - 1 + 1)) + 1
 
-score = 20
+let score = 20;
+console.log(random)
 
+
+let checkButton = document.querySelector(".check")
+
+checkButton.addEventListener("click", 
+    function () {
+
+        let guess = document.querySelector(".text-box").value;
+        console.log(guess)
+
+        if (guess == ""){
+            document.querySelector(".message").textContent = "⛔️ Not a number!"
+        }
+        else if (guess != random) {
+            if (guess < random) {
+                document.querySelector(".message").textContent = "📉 Too low!"
+            }
+            else if(guess > random) {
+                document.querySelector(".message").textContent = "📈 Too high"
+            }
+        }
+        else{
+            document.querySelector("message").textContent = "🎉You win! "
+        }
+
+
+    })
